30daa7cb189d38cea510cbfdf86c9a03815f9097
salmanmasroor <salmanmasroor07@gmail.com>
2026-09-21 20:31:16 +0500

    create

--- ddl.sql
+++ ddl.sql
@@ -3,3 +3,13 @@ SELECT datname FROM pg_database
 
 --create database
 CREATE DATABASE test;
+
+--delete database
+DROP DATABASE schooldb;
+
+--create table
+CREATE TABLE person(
+	id INTEGER,
+	name VARCHAR(50),
+	city VARCHAR(50)
+)
